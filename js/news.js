5d8da6a16620e9f40184b171c7be6b1f51e6dbe8
RRO (RSL-Md. Motiur Rahman) <RRO@rahimafrooz.com>
2022-09-03 13:13:36 +0600

    add total view

--- js/news.js
+++ js/news.js
@@ -24,7 +24,7 @@ function displayCategory(data) {
 
 
 
-
+// Call Based on category ID
 function newsID(catId) {
     //console.log(catId);
     let url = `https://openapi.programming-hero.com/api/news/category/${catId}`;
@@ -38,6 +38,8 @@ const newsInfo = (url) => {
         .then(data => displayNews(data.data));
 }
 
+
+// Display News
 function displayNews(data) {
     console.log(data);
     const newsItems = document.getElementById('newsItems');
@@ -58,7 +60,22 @@ function displayNews(data) {
                 text-overflow: ellipsis;">
                     <h5 class="card-title">${element.title}</h5>
                     <p class="card-text">${element.details}</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                   <div class="d-flex">
+                        <div>
+                            <img src="${element.author.img}" width="20px" height="20px" class="rounded" alt="...">
+                        </div>
+                        <div>
+                            <h6 id="name">${element.author.name ? element.author.name : "Not Found"}</h6>
+                            <p id="publishDae">${element.author.published_date ? element.author.published_date : "Not Found"}
+                            </p>
+                        </div>
+                       
+                        <div class="d-flex">
+                            <img  width="20px" height="20px" src="" class="rounded" alt="...">
+                            <p>${element.total_view}</p>
+                        </div>
+                      
+                    </div>
                 </div>
             </div>
         </div>
