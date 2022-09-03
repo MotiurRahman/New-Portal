43900684639875c73714283bacc3e95adaa3a2e8
RRO (RSL-Md. Motiur Rahman) <RRO@rahimafrooz.com>
2022-09-03 12:56:51 +0600

    update card

--- js/news.js
+++ js/news.js
@@ -14,7 +14,7 @@ function displayCategory(data) {
         const li = document.createElement('li');
         li.classList.add('nav-item');
         const catName = `
-        <a class="nav-link mx-3" href="#" onclick="newID(${element.category_id})">${element.category_name}</a>
+        <a class="nav-link mx-3" href="#" onclick="newsID('${element.category_id}')">${element.category_name}</a>
         `
         li.innerHTML = catName;
         categoryList.appendChild(li);
@@ -23,38 +23,51 @@ function displayCategory(data) {
 }
 
 
+
+
+function newsID(catId) {
+    //console.log(catId);
+    let url = `https://openapi.programming-hero.com/api/news/category/${catId}`;
+    newsInfo(url);
+
+}
+
 const newsInfo = (url) => {
     fetch(url)
         .then(res => res.json())
-        .then(data => displayNews(data));
-}
-
-function newID(catId) {
-    let url = `https://openapi.programming-hero.com/api/news/category/${catId}`;
-    newsInfo(url);
-    console.log(catId);
+        .then(data => displayNews(data.data));
 }
 
 function displayNews(data) {
     console.log(data);
     const newsItems = document.getElementById('newsItems');
-    const news = `
-    <div class="card mb-3" style="max-width: 540px;">
-    <div class="row g-0">
-        <div class="col-md-4">
-            <img src="..." class="img-fluid rounded-start" alt="...">
-        </div>
-        <div class="col-md-8">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural
-                    lead-in to
-                    additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    newsItems.innerHTML = "";
+    data.forEach(element => {
+        const newsCard = document.createElement('div');
+        newsCard.classList.add("card");
+        newsCard.classList.add("mb-3");
+        newsCard.classList.add("border-0");
+        const news = `
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img src="${element.image_url}" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8 p-2">
+                <div class="card-body" style="max-height:300px;
+                overflow: hidden !important;
+                text-overflow: ellipsis;">
+                    <h5 class="card-title">${element.title}</h5>
+                    <p class="card-text">${element.details}</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
             </div>
         </div>
-    </div>
-</div>
-    `
+    
+        `
+
+        newsCard.innerHTML = news;
+        newsItems.appendChild(newsCard);
+    });
+
 
 }
