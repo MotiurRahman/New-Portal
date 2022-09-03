b0ffe96bb32e7b4e0f9a60a9c38cfd603190b7e2
RRO (RSL-Md. Motiur Rahman) <RRO@rahimafrooz.com>
2022-09-03 15:48:38 +0600

    update category code

--- js/news.js
+++ js/news.js
@@ -32,6 +32,7 @@ function newsID(catId) {
 
 }
 
+
 const newsInfo = (url) => {
     fetch(url)
         .then(res => res.json())
@@ -39,6 +40,11 @@ const newsInfo = (url) => {
 }
 
 
+//Initial loading
+function loadNews() {
+    newsInfo("https://openapi.programming-hero.com/api/news/category/01");
+}
+loadNews();
 // Display News
 function displayNews(data) {
     console.log(data);
@@ -51,28 +57,31 @@ function displayNews(data) {
         newsCard.classList.add("border-0");
         const news = `
         <div class="row g-0">
-            <div class="col-md-4">
+            <div class="col-md-4 col-12 p-2">
                 <img src="${element.image_url}" class="img-fluid rounded-start" alt="...">
             </div>
-            <div class="col-md-8 p-2">
-                <div class="card-body" style="max-height:300px;
-                overflow: hidden !important;
-                text-overflow: ellipsis;">
-                    <h5 class="card-title">${element.title}</h5>
-                    <p class="card-text">${element.details}</p>
-                   <div class="d-flex">
-                        <div>
+            <div class="col-md-8 col-12">
+            <div class="d-flex flex-column" style="max-height:300px;">
+                    <article class="p-2">
+                        <h5 class="card-title">${element.title}</h5>
+                        <p class="card-text">${element.details.slice(0, 400)}</p>
+                    </article>
+                   <div class="d-flex align-items-center">
+                      <div class="d-flex align-items-center flex-fill">
+                        <div class="align-items-center">
                             <img src="${element.author.img}" width="20px" height="20px" class="rounded" alt="...">
                         </div>
-                        <div>
+                        <div class="align-items-center mx-2" style="">
                             <h6 id="name">${element.author.name ? element.author.name : "Not Found"}</h6>
-                            <p id="publishDae">${element.author.published_date ? element.author.published_date : "Not Found"}
-                            </p>
                         </div>
-                       
-                        <div class="d-flex">
+                      </div>                     
+                        <div class="align-items-center d-flex flex-fill">
                             <img  width="20px" height="20px" src="" class="rounded" alt="...">
-                            <p>${element.total_view}</p>
+                            <p class="mx-2">${element.total_view}</p>
+                        </div>
+
+                        <div class="align-items-center flex-fill">
+                          <button class="btn btn-success" onclick="moreDetails('${element._id}')">More detils</button>
                         </div>
                       
                     </div>
